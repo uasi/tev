8b70404c02f57f66bb265ed0dd2adab0324cd4c7
Tomoki Aonuma <uasi@uasi.jp>
2015-12-24 23:10:29 +0900

    Configure for production

--- config/prod.exs
+++ config/prod.exs
@@ -13,53 +13,15 @@ use Mix.Config
 # which you typically run after static files are built.
 config :tev, Tev.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "tev.opts.io", port: 80]
 
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :tev, Tev.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :tev, Tev.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+config :tev, Tev.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :tev, Tev.Endpoint, server: true
-#
-# You will also need to set the application root to `.` in order
-# for the new static assets to be served after a hot upgrade:
-#
-#     config :tev, Tev.Endpoint, root: "."
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :tev, Tev.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
