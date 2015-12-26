e9a1e813dca005e0db95eb10e07eda0c056ec946
Tomoki Aonuma <uasi@uasi.jp>
2015-12-26 22:20:42 +0900

    Add :current_user and :authorize helper to Tev.Auth

--- web/auth.ex
+++ web/auth.ex
@@ -1,21 +1,12 @@
 defmodule Tev.Auth do
   @moduledoc """
-  Provides auth plugs and a controller helper which injects current user into
-  arguments to action.
-
-  ## Examples
-
-      defmodule MyController do
-        use Tev.Auth, :current_user
-
-        def index(conn, params, current_user) do
-        end
-      end
+  Provides controller helpers for authentication/authorization.
   """
 
   alias Tev.Session
   alias Tev.UnauthorizedError
 
+  @doc false
   @spec authenticate(Plug.Conn.t, Plug.opts) :: Plug.Conn.t
   def authenticate(conn, _opts) do
     case Session.fetch_current_user(conn) do
@@ -28,6 +19,7 @@ defmodule Tev.Auth do
     end
   end
 
+  @doc false
   @spec authorize(Plug.Conn.t, Plug.opts) :: Plug.Conn.t | no_return
   def authorize(conn, _opts) do
     if Session.signed_in?(conn) do
@@ -37,6 +29,65 @@ defmodule Tev.Auth do
     end
   end
 
+  @doc """
+  `use Tev.Auth, :current_user` injects current user into arguments to action
+  function.
+
+  If current user has been set by `Session.put_current_user/1`, fetch it and
+  inject as the third argument to action function. Otherwise set nil to the
+  third argument.
+
+  ## Examples
+
+      defmodule MyController do
+        use Tev.Auth, :current_user
+
+        def index(conn, params, current_user) do
+        end
+      end
+
+  `use Tev.Auth, authorize: default_boolean` controls authorization.
+
+  An action which function is preceded by `@authorize true` requires
+  `conn.assigns[:current_user]` to be set to non-nil. If this requirement isn't
+  met, the action will raise `Tev.UnauthorizedError`.
+  Precede an action function by `@authorize false` to disable this behavior.
+
+  If neither attribute is defined for an action function, it will be assumed
+  that the function is preceded by `@authorize default_boolean` where
+  `default_boolean` is the value given as
+  `use Auth.Tev, authorize: default_boolean`.
+
+  ## Examples
+
+      defmodule MyController1 do
+        use Tev.Auth, authorize: false
+
+        def unclassified(conn, _params) do
+          ...
+        end
+
+        @authorize true
+        def confidential(conn, _params) do
+          # Raises Tev.UnauthorizedError unless conn.assigns[:current_user] is set.
+        end
+      end
+
+      defmodule MyController2 do
+        use Tev.Auth, authorize: true
+
+        @authorize false
+        def unclassified(conn, _params) do
+          ...
+        end
+
+        def confidential(conn, _params) do
+          # Raises Tev.UnauthorizedError unless conn.assigns[:current_user] is set.
+        end
+      end
+  """
+  @spec __using__(:current_user) :: term
+  @spec __using__([authorize: boolean]) :: term
   defmacro __using__(:current_user) do
     quote do
       import unquote(__MODULE__)
@@ -47,6 +98,28 @@ defmodule Tev.Auth do
         apply(__MODULE__, action_name(conn),
               [conn, conn.params, conn.assigns[:current_user]])
       end
+    end
+  end
+  defmacro __using__([authorize: default]) do
+    quote do
+      import unquote(__MODULE__)
+
+      Module.register_attribute(__MODULE__, :authorize, accumulate: true)
+
+      @authorize unquote(default)
+      @on_definition {unquote(__MODULE__), :put_authorize_plug}
+    end
+  end
+
+  @doc false
+  def put_authorize_plug(env, _kind, name, _args, _guards, _body) do
+    attrs = Module.get_attribute(env.module, :authorize)
+    default = List.last(attrs)
+    authorize = List.first(attrs)
+    if authorize do
+      plug = {:authorize, [], quote(do: var!(action) == unquote(name))}
+      Module.put_attribute(env.module, :plugs, plug)
+      Module.put_attribute(env.module, :authorize, default)
     end
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,14 +1,14 @@
 defmodule Tev.PageController do
   use Tev.Web, :controller
+  use Tev.Auth, authorize: false
   use Tev.Auth, :current_user
-
-  plug :authorize when action in [:confidential]
 
   def index(conn, _params, user) do
     id_str = if user, do: user.twitter_id_str, else: nil
     render conn, "index.html", id_str: id_str
   end
 
+  @authorize true
   def confidential(conn, _params, _user) do
     text conn, "ok"
   end
