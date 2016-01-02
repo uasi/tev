21ab923b1c616baa70709ff5a55650c932bbd279
Tomoki Aonuma <uasi@uasi.jp>
2016-01-02 19:28:24 +0900

    Upgrade Elixir to 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tev.Mixfile do
   def project do
     [app: :tev,
      version: "0.0.1",
-     elixir: "~> 1.1",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
