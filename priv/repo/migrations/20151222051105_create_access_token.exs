a23caed18316ca405e48ef823147510f52dce6af
Tomoki Aonuma <uasi@uasi.jp>
2016-01-22 19:51:37 +0900

    Update CreateAccessToken migration

--- priv/repo/migrations/20151222051105_create_access_token.exs
+++ priv/repo/migrations/20151222051105_create_access_token.exs
@@ -3,13 +3,13 @@ defmodule Tev.Repo.Migrations.CreateAccessToken do
 
   def change do
     create table(:access_tokens) do
-      add :oauth_token_encrypted, :binary
-      add :oauth_token_secret_encrypted, :binary
-      add :user_id, references(:users)
+      add :oauth_token_encrypted, :binary, null: false
+      add :oauth_token_secret_encrypted, :binary, null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
 
       timestamps
     end
-    create index(:access_tokens, [:user_id])
+    create unique_index(:access_tokens, [:user_id])
 
   end
 end
