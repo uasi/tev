c6cae5170a909277cab67aeed4070302abb48756
Tomoki Aonuma <uasi@uasi.jp>
2016-01-27 01:24:17 +0900

    Fix existing migrations

--- priv/repo/migrations/20151222051105_create_access_token.exs
+++ priv/repo/migrations/20151222051105_create_access_token.exs
@@ -5,7 +5,7 @@ defmodule Tev.Repo.Migrations.CreateAccessToken do
     create table(:access_tokens) do
       add :oauth_token_encrypted, :binary, null: false
       add :oauth_token_secret_encrypted, :binary, null: false
-      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :user_id, references(:users, type: :bigint, on_delete: :delete_all), null: false
 
       timestamps
     end
--- priv/repo/migrations/20160122120747_create_home_timeline.exs
+++ priv/repo/migrations/20160122120747_create_home_timeline.exs
@@ -4,7 +4,7 @@ defmodule Tev.Repo.Migrations.CreateHomeTimeline do
   def change do
     create table(:home_timelines) do
       add :max_tweet_id, :bigint
-      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :user_id, references(:users, type: :bigint, on_delete: :delete_all), null: false
 
       timestamps
     end
