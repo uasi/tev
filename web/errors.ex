c13c1949184b62bcbbf28b06576bcdecde8f234f
Tomoki Aonuma <uasi@uasi.jp>
2016-02-06 23:21:04 +0900

    Add spec to HTTP error functions

--- web/errors.ex
+++ web/errors.ex
@@ -1,8 +1,10 @@
 defmodule Tev.Errors do
+  @spec raise_unauthorized([term]) :: no_return
   def raise_unauthorized(attrs \\ []) do
     raise Tev.UnauthorizedError, attrs
   end
 
+  @spec raise_forbidden([term]) :: no_return
   def raise_forbidden(attrs \\ []) do
     raise Tev.ForbiddenError, attrs
   end
