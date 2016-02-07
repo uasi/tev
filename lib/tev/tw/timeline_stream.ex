77549933f40d3c5fe3b45a4fe949e2a19f7a96e8
Tomoki Aonuma <uasi@uasi.jp>
2016-02-08 00:21:36 +0900

    Put limit on TimelineStream

--- lib/tev/tw/timeline_stream.ex
+++ lib/tev/tw/timeline_stream.ex
@@ -6,9 +6,11 @@ defmodule Tev.Tw.TimelineStream do
   Tweets are usually sorted by newest first order, though not guaranteed.
   """
 
-  @type timeline_opt :: {:max_id, integer} | {:since_id, integer}
+  @type timeline_opt :: {:max_id, integer} | {:since_id, integer} | {:max_requests, integer}
   @type timeline_ret :: [ExTwitter.Model.Tweet.t] | no_return
   @typep timeline_type :: :home | :like
+
+  @default_max_requests 15
 
   @doc """
   Returns a stream of all available tweets in a timeline.
@@ -21,16 +23,20 @@ defmodule Tev.Tw.TimelineStream do
   def timeline(type, opts) do
     default_max_id = Keyword.get(opts, :max_id)
     since_id = Keyword.get(opts, :since_id)
+    max_requests = Keyword.get(opts, :max_requests, @default_max_requests)
 
     Stream.resource(
-      fn -> nil end,
-      fn last_tweet ->
-        max_id = if last_tweet, do: last_tweet.id - 1, else: default_max_id
-        case get_tweets(type, [max_id: max_id, since_id: since_id]) do
-          [] ->
-            {:halt, nil}
-          tweets ->
-            {tweets, List.last(tweets)}
+      fn -> {nil, max_requests} end,
+      fn
+        {_, 0} ->
+          {:halt, nil}
+        {last_tweet, remaining_requests} ->
+          max_id = if last_tweet, do: last_tweet.id - 1, else: default_max_id
+          case get_tweets(type, [max_id: max_id, since_id: since_id]) do
+            [] ->
+              {:halt, nil}
+            tweets ->
+              {tweets, {List.last(tweets), remaining_requests - 1}}
         end
       end,
       fn _ -> nil end
